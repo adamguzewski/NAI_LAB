bf52d5da125cda5cae67884915abbb61a0cc78d0
Adam <mail2gzs@gmail.com>
2021-11-06 00:54:34 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,92 @@
+"""
+***********************************************
+FUZZY CONTROL SYSTEM: Insurance Risk Assessment
+***********************************************
+Author: Adam Gużewski
+
+To run program install:
+pip install scikit-fuzzy |
+pip install matplotlib |
+
+I created a fuzzy control system which calculates the risk of insurance based on drivers age, population of the city
+when driver lives and power of drivers car.
+
+**********************************************
+
+-- Antecednets (Inputs):
+    1) 'drivers_age' - values between 18 and 80 // How old is the customer
+        Fuzzy set: low, medium, high
+    2)  'population_of_city' - values between 0 and 500 (expressed in 10 000, e.g. 'population_of_city' = 150, it means
+        1 500 000 in real life) // how big is the population in drivers city
+        Fuzzy set: low, medium, high
+    3)  'car_power' - values between 0 and 220 (kilowatt) // how much power drivers car has
+        Fuzzy set: low, medium, high
+
+-- Consequents (Outputs):
+    1) 'risk'
+    Universe: How risky is our client on a scale from 0 to 100% ?
+    Fuzzy Set: low, medium, high
+
+
+
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+drivers_age = ctrl.Antecedent(np.arange(18, 81, 1), 'drivers_age')
+population_of_city = ctrl.Antecedent(np.arange(0, 501, 1), 'population_of_city')
+car_power = ctrl.Antecedent(np.arange(0, 221, 1), 'car_power')
+
+risk = ctrl.Consequent(np.arange(0, 101, 1), 'insurance_risk')
+
+drivers_age['low'] = fuzz.trimf(drivers_age.universe, [18, 24, 31])
+drivers_age['medium'] = fuzz.trimf(drivers_age.universe, [25, 34, 50])
+drivers_age['high'] = fuzz.trimf(drivers_age.universe, [31, 50, 80])
+
+population_of_city['low'] = fuzz.trimf(population_of_city.universe, [0, 150, 250])
+population_of_city['medium'] = fuzz.trimf(population_of_city.universe, [150, 250, 350])
+population_of_city['high'] = fuzz.trimf(population_of_city.universe, [250, 350, 500])
+
+car_power['low'] = fuzz.trimf(car_power.universe, [0, 70, 120])
+car_power['medium'] = fuzz.trimf(car_power.universe, [70, 120, 170])
+car_power['high'] = fuzz.trimf(car_power.universe, [120, 170, 220])
+
+
+# drivers_age.automf(3)
+# population_of_city.automf(3)
+# car_power.automf(3)
+
+risk['low'] = fuzz.trimf(risk.universe, [0, 25, 50])
+risk['medium'] = fuzz.trimf(risk.universe, [25, 50, 75])
+risk['high'] = fuzz.trimf(risk.universe, [50, 75, 100])
+
+risk.view()
+population_of_city.view()
+car_power.view()
+drivers_age.view()
+
+rule1 = ctrl.Rule(drivers_age['low'] | population_of_city['high'] | car_power['high'], risk['high'])
+rule2 = ctrl.Rule(drivers_age['low'] | population_of_city['low'] | car_power['low'], risk['medium'])
+rule3 = ctrl.Rule(drivers_age['high'] | population_of_city['low'] | car_power['low'], risk['medium'])
+rule4 = ctrl.Rule(drivers_age['high'] | population_of_city['low'] | car_power['high'], risk['high'])
+rule5 = ctrl.Rule(drivers_age['medium'] | population_of_city['low'] | car_power['high'], risk['medium'])
+
+insurance_risk_control = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
+
+insurance_risk = ctrl.ControlSystemSimulation(insurance_risk_control)
+
+insurance_risk.input['drivers_age'] = 35
+insurance_risk.input['population_of_city'] = 450
+insurance_risk.input['car_power'] = 100
+
+insurance_risk.compute()
+
+print(insurance_risk.output['insurance_risk'])
+risk.view(sim=insurance_risk)
+
+plt.show()
+
